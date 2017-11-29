f449e9aa213a67529f9e99a2dba8a423311a5dc9
何浩一 <hehe790223@gmail.com>
2017-11-29 18:39:39 +0800

    implement multi-layer Neural Network
    can set L layer
    layer[0] :  number of data's eigenvalues (input layer)
    layer[L-1] : number of data's output index (output layer)
    
    EX.
    layers_dims = [X_train.shape[0], 40, 30, 20, 10, Y_train.shape[0]]
    have 4 hidden layer, 
    1st have 40 parameters
    2nd have 30 parameters
    3rd have 20 parameters
    4th have 10 parameters

--- 3_NeuralNetwork/NeuralNetwork.py
+++ 3_NeuralNetwork/NeuralNetwork.py
@@ -2,6 +2,24 @@ import numpy as np
 from numpy import genfromtxt
 
 
+def relu(Z):
+    A = Z*(Z>0)
+    return A,Z
+def sigmoid(Z):
+    A = 1/(1+np.exp(-Z))
+    return A,Z
+def relu_prime(Z):
+    temp = 1*(Z>0)
+    return temp
+def sigmoid_prime(Z):
+    a = 1/(1+np.exp(-Z))
+    a = a*(1-a)
+    return a
+def cost_prime(AL,Y):
+    temp = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))  #may occur divide 0 error
+    return temp
+
+
 """
 X = genfromtxt('titanic_train_x.txt')
 X = X.T   #X would be look like (number of eigenvalues, number of datas)
@@ -18,140 +36,142 @@ X_test = genfromtxt("regularization_test_x.txt")
 Y_test = genfromtxt("regularization_test_y.txt")
 Y_test.resize(1,Y_test.shape[0])
 
-
-def layer_size(X,Y,hidden_layer):
+def initialize_parameters(layers_dims):
     """
-    Arguments:
-    X -- input dataset of shape (input size, number of examples)
-    Y -- labels of shape (output size, number of examples)
-    Returns:
-    n_x -- the size of the input layer, number of eigenvalues
-    n_h -- the size of the hidden layer, number of layer_1
-    n_y -- the size of the output layer
+    layer_dims -- python array (list) containing the dimensions of each layer in our network
     """
-    n_x = X.shape[0] # size of input layer
-    n_h = hidden_layer
-    n_y = Y.shape[0] # size of output layer
-
-    return n_x, n_h, n_y
-
-def initial_parameters(n_x,n_h,n_y):
-
-    W1 = np.random.randn(n_h,n_x)*0.01  #If not initialize randomly, it's only linear fit repeat n_h times.
-    b1 = np.zeros((n_h,1))
-    W2 = np.random.randn(n_y,n_h)*0.01
-    b2 = np.zeros((n_y,1))
-
-    parameters = {"W1": W1,
-                  "b1": b1,
-                  "W2": W2,
-                  "b2": b2}
+    L = len(layers_dims)
+    parameters = {}
+    for i in range(1,L):
+        #((2/layers_dims[i-1])**(1/2)) initialize W properly can make Z (Z=WA+b) at same scale in every layer.
+        parameters["W"+str(i)] = np.random.randn(layers_dims[i],layers_dims[i-1])*((2/layers_dims[i-1])**(1/2))
+        parameters["b"+str(i)] = np.zeros((layers_dims[i],1))
     return parameters
 
-def forward_propagation(X,parameters):
-    
-    W1 = parameters["W1"]
-    b1 = parameters["b1"]
-    W2 = parameters["W2"]
-    b2 = parameters["b2"]
+def linear_forward(A_prev, W, b):
 
-    Z1 = np.dot(W1,X)+b1
-    A1 = np.tanh(Z1)
-    Z2 = np.dot(W2,A1)+b2
-    A2 = 1/(1+np.exp(-Z2))
+    Z = np.dot(W,A_prev)+b
+    linear_cache = A_prev,W,b
 
-    cache = {"Z1": Z1,
-             "A1": A1,
-             "Z2": Z2,
-             "A2": A2}
-    return A2, cache
+    return Z,linear_cache
 
+def linear_activation_forward(A_prev, W, b, activation):
 
-def compute_cost(A2, Y, parameters):
+    Z, linear_cache = linear_forward(A_prev,W,b)
+
+    if activation == "sigmoid":
+        A, activation_cache = sigmoid(Z)
+    elif activation == "relu":
+        A, activation_cache = relu(Z)
+
+    cache = linear_cache, activation_cache
+
+    return A, cache
+
+def L_model_forward(X, parameters):
+
+    caches = []
+    L = len(parameters)//2
+    A = X
+    for i in range(1,L):
+        A_prev = A
+        A,cache = linear_activation_forward(A_prev, parameters["W"+str(i)], parameters["b"+str(i)], "relu")
+        caches.append(cache)
+
+    A_prev = A
+    AL,cache = linear_activation_forward(A_prev, parameters["W"+str(L)], parameters["b"+str(L)], "sigmoid")
+    caches.append(cache)
+
+    return AL,caches
+
+def compute_cost(AL, Y):
 
     m = Y.shape[1]
-    cost = np.sum(-1*(Y*np.log(A2)+(1-Y)*np.log(1-A2)))/m
-    cost = np.squeeze(cost)     # makes sure cost is the dimension we expect. 
-                                # E.g., turns [[17]] into 17 
+    cost = np.sum(-1*(Y*np.log(AL)+(1-Y)*np.log(1-AL)))/m    
+    cost = np.squeeze(cost)      # To make sure cost's shape is what we expect (e.g. this turns [[17]] into 17).
+
     return cost
 
+def linear_backward(dZ, linear_cache):
 
-def backward_propagation(parameters, cache, X, Y):
-
-    m = X.shape[1]
+    A_prev,W,b = linear_cache
+    m = A_prev.shape[1]
     
-    W1 = parameters["W1"]
-    W2 = parameters["W2"]
+    dW = np.dot(dZ,A_prev.T)/m
+    db = np.sum(dZ, axis=1, keepdims=True)/m
+    dA_prev = np.dot(W.T,dZ)
 
-    A1 = cache["A1"]
-    A2 = cache["A2"]
+    return dA_prev,dW,db
 
-    dZ2 = A2 - Y
-    dW2 = np.dot(dZ2,A1.T)/m
-    db2 = np.sum(dZ2,axis = 1,keepdims = True)/m
-    dZ1 = np.dot(W2.T,dZ2)*(1-np.power(A1,2))
-    dW1 = np.dot(dZ1,X.T)/m
-    db1 = np.sum(dZ1,axis = 1,keepdims = True)/m
+def linear_activation_backward(dA, cache, activation):
 
-    grads = {"dW1": dW1,
-             "db1": db1,
-             "dW2": dW2,
-             "db2": db2}
+    linear_cache, activation_cache = cache
+    Z = activation_cache
+
+    if activation=="relu":
+        dZ = dA*relu_prime(Z)
+    elif activation=="sigmoid":
+        dZ = dA*sigmoid_prime(Z)
+    
+    dA_prev,dW,db = linear_backward(dZ,linear_cache)
+
+    return dA_prev,dW,db
+
+def L_model_backward(AL, Y, caches):
+
+    grads = {}
+    L = len(caches)
+
+    dAL = cost_prime(AL,Y)
+    dA_prev, dW, db = linear_activation_backward(dAL, caches[L-1], "sigmoid")
+    grads["dW"+str(L)] = dW
+    grads["db"+str(L)] = db
+
+    for i in reversed(range(1,L)):
+        dA = dA_prev
+        dA_prev, dW, db = linear_activation_backward(dA, caches[i-1], "relu")
+        grads["dW"+str(i)] = dW
+        grads["db"+str(i)] = db
+
     return grads
 
 def update_parameters(parameters, grads, learning_rate):
 
-    W1 = parameters["W1"]
-    b1 = parameters["b1"]
-    W2 = parameters["W2"]
-    b2 = parameters["b2"]
+    L = len(parameters) // 2 
 
-    dW1 = grads["dW1"]
-    db1 = grads["db1"]
-    dW2 = grads["dW2"]
-    db2 = grads["db2"]
-
-    W1 -= learning_rate*dW1
-    b1 -= learning_rate*db1
-    W2 -= learning_rate*dW2
-    b2 -= learning_rate*db2
-
-    parameters = {"W1": W1,
-                  "b1": b1,
-                  "W2": W2,
-                  "b2": b2}
+    for i in range(L):
+        parameters["W"+str(i+1)] -= learning_rate*grads["dW"+str(i+1)]
+        parameters["b"+str(i+1)] -= learning_rate*grads["db"+str(i+1)]
 
     return parameters
 
-def predict(parameters, X):
+def predict(X, parameters):
 
-    A2, cache = forward_propagation(X, parameters)
-    predictions = (A2 > 0.5) #A2 is sigmoid function, a number bwtween 0 and 1
+    AL, cache = L_model_forward(X,parameters)
+    predictions = 1*(AL > 0.5) #AL is sigmoid function, output bwtween 0 and 1
 
     return predictions
 
-def nn_model(X, Y, n_h, num_iterations, learning_rate, print_cost):
+def L_layer_model(X, Y, layers_dims, learning_rate, num_iterations, print_cost):
 
-    n_x,n_h,n_y = layer_size(X,Y,n_h)
-    parameters = initial_parameters(n_x,n_h,n_y)
-    
+    np.random.seed(1)
+    parameters = initialize_parameters(layers_dims)
+
     for i in range(num_iterations):
-
-        A2,cache = forward_propagation(X,parameters)
-
-        if print_cost and i%1000==0 :
-            cost = compute_cost(A2,Y,parameters)
+        AL,caches = L_model_forward(X,parameters)
+        if print_cost and i%1000==0:
+            cost = compute_cost(AL,Y)
             print("cost after %6d iterations:%.3f" %(i,cost))
-
-        grads = backward_propagation(parameters,cache,X,Y)
-
-        parameters = update_parameters(parameters,grads,learning_rate)
-
+        grads = L_model_backward(AL,Y,caches)
+        parameters = update_parameters(parameters, grads, learning_rate)
 
     return parameters
 
-parameters = nn_model(X_train,Y_train, n_h=2, num_iterations=10000, learning_rate=0.01, print_cost=True)
-prediction = predict(parameters,X_train)
-print("train accuracy: {} %".format(100 - np.mean(np.abs(prediction - Y_train)) * 100))
-prediction = predict(parameters,X_test)
 
+
+layers_dims = [X_train.shape[0],40,30,20,10,Y_train.shape[0]]
+parameters = L_layer_model(X_train, Y_train, layers_dims, learning_rate=0.01, num_iterations=200000, print_cost=True)
+prediction = predict(X_train,parameters)
+print("train accuracy: {} %".format(100 - np.mean(np.abs(prediction - Y_train)) * 100))
+prediction = predict(X_test,parameters)
+print("test accuracy: {} %".format(100 - np.mean(np.abs(prediction - Y_test)) * 100))
